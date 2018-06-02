f54af89f545afca503364f65c5ea289216a29dec
Yury Staravoitau <MorfinAll@mail.ru>
2018-06-03 02:16:23 +0300

    gameManager/feature: Immplemented draft basic functional for GameManager class

--- src/js/modules/gameManager/gameManager.js
+++ src/js/modules/gameManager/gameManager.js
@@ -1,7 +1,40 @@
-class GameManager {
+export default class GameManager {
+	constructor() {
+		console.log('GameManager: created new GameManager');
+		this.monsterGroupsCounter = 0;
+	}
+
+	startGame() {
+		console.log('GameManager: startGame()');
+		let playerGroup = this.genericUnitGroup('player');
+		console.log(playerGroup);
+		while (this.isPlayerAlive()) {
+			let monsterGroup = this.genericUnitGroup('monster' + this.monsterGroupsCounter);
+			console.log(monsterGroup);
+		}
+	}
+
+	keydown(action) {
+		console.log('GameManager: player keydown key ' + action);
+	}
+
+	keyup(action) {
+		console.log('GameManager: player keyup key ' + action);
+	}
+
+	genericUnitGroup(typeGroup) {
+		let unitGroup = [];
+		unitGroup.nameGroup = typeGroup;
+		for (let i = 0; i < 3; i++) {
+			unitGroup.push({ 'unit': 'unit number ' + i });
+		}
+		return unitGroup;
+	}
+
+	isPlayerAlive() {
+		this.monsterGroupsCounter++;
+		if (this.monsterGroupsCounter > 5) return false;
+		return true;
+	}
 
 }
-
-
-const sayHi = () => console.log('module GameManager connected');
-export default sayHi;
