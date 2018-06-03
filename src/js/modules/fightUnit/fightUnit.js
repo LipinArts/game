c2a471bae98b5c59f135f6bc310fa12866ad85e1
Artsiom Lipin <lipinartsiom@gmail.com>
2018-06-03 15:08:17 +0300

    figthUnit/feature: implemented unit name generate fucntions

--- src/js/modules/fightUnit/fightUnit.js
+++ src/js/modules/fightUnit/fightUnit.js
@@ -1,88 +1,93 @@
 import _ from 'lodash';
 import unit from '../../unitConfig';
+
 export default class FightUnit {
 	constructor(unit, name, difficulty) {
-		this.unit = unit ? unit : 'monster'; // str (флаг) - 'player' или 'monster'(если не указать - отрисовывает монстра)
-		this.unitName = name; // str - имя юнита - если передать(при флаге unit = 'player') - то это будет имя игрока, если не передать - то сгеннерируется случайно; (при флаге unit = 'monster' - проигнорируется)(необязательный)
-		this.unitHP;
-		this.damage;
-		this.unitAbilities = {};
-		this.unitSprites = {};
+		this.unit = unit ? unit : 'monster';
+		this.name = name;
+		this.hp = '';
+		this.damage = '';
+		this.abilities = {};
+		this.sprites = {};
+		this.generate();
+	}
+
+	generate() {
 		switch (unit) {
 		case 'player':
-			this.generatePlayerName();
+			this.generatePlayerUnit();
 			break;
 		case 'monster':
-			this.generateMonsterName();
 			this.generateMonsterUnit();
 			break;
 		default:
-			this.generateMonsterName();
 			this.generateMonsterUnit();
-			this.render();
 			break;
 		}
 	}
 
-	generateMonsterUnit() { // генерирует юнита монстра случайно скомбинированного из частей(спрайты)
-		this.unitSprites.head = {
-			path: unit.monsters.heads_path, // путь к спрайту
-			width: unit.monsters.heads_width,
-			height: unit.monsters.heads_height,
-			bgPosX: unit.monsters.heads_bg_posX[_.random(0, unit.monsters.heads_bg_posX.length)], // рандомный выбор головы из спрайта по координатам X
-			bgPosY: unit.monsters.heads_bg_posY, // координаты головы спрайта по Y
-		};
-		this.unitSprites.body = {
-			path: unit.monsters.bodies_path,
-			width: unit.monsters.bodies_width,
-			height: unit.monsters.bodies_height,
-			bgPosX: unit.monsters.bodies_bg_posX[_.random(0, unit.monsters.bodies_bg_posX.length)],
-			bgPosY: unit.monsters.bodies_bg_posY,
-		};
-		this.unitSprites.hands = {
-			path: unit.monsters.hands_path,
-			width: unit.monsters.hands_width,
-			height: unit.monsters.hands_height,
-			bgPosX: unit.monsters.hands_bg_posX[_.random(0, unit.monsters.hands_bg_posX.length)],
-			bgPosY: unit.monsters.hands_bg_posY,
-		};
-		this.unitSprites.legs = {
-			path: unit.monsters.hands_path,
-			width: unit.monsters.hands_width,
-			height: unit.monsters.hands_height,
-			bgPosX: unit.monsters.hands_bg_posX[_.random(0, unit.monsters.hands_bg_posX.length)],
-			bgPosY: unit.monsters.hands_bg_posY,
-		};
+	generateMonsterUnit() {
+		this.generateMonsterName();
+
+		// this.unitSprites.head = {
+		// 	path: unit.monsters.heads_path, // путь к спрайту
+		// 	width: unit.monsters.heads_width,
+		// 	height: unit.monsters.heads_height,
+		// 	bgPosX: unit.monsters.heads_bg_posX[_.random(0, unit.monsters.heads_bg_posX.length)], // рандомный выбор головы из спрайта по координатам X
+		// 	bgPosY: unit.monsters.heads_bg_posY, // координаты головы спрайта по Y
+		// };
+		// this.unitSprites.body = {
+		// 	path: unit.monsters.bodies_path,
+		// 	width: unit.monsters.bodies_width,
+		// 	height: unit.monsters.bodies_height,
+		// 	bgPosX: unit.monsters.bodies_bg_posX[_.random(0, unit.monsters.bodies_bg_posX.length)],
+		// 	bgPosY: unit.monsters.bodies_bg_posY,
+		// };
+		// this.unitSprites.hands = {
+		// 	path: unit.monsters.hands_path,
+		// 	width: unit.monsters.hands_width,
+		// 	height: unit.monsters.hands_height,
+		// 	bgPosX: unit.monsters.hands_bg_posX[_.random(0, unit.monsters.hands_bg_posX.length)],
+		// 	bgPosY: unit.monsters.hands_bg_posY,
+		// };
+		// this.unitSprites.legs = {
+		// 	path: unit.monsters.hands_path,
+		// 	width: unit.monsters.hands_width,
+		// 	height: unit.monsters.hands_height,
+		// 	bgPosX: unit.monsters.hands_bg_posX[_.random(0, unit.monsters.hands_bg_posX.length)],
+		// 	bgPosY: unit.monsters.hands_bg_posY,
+		//	};
 
 	}
 
-	generatePlayerUnit() {} // генерирует юнита игрока аналогично монстрам.
-
-	generateMonsterName() { // генерируем случайное имя юнита монстра
-		const firstName = unit.bad_adjectives[_.random(0, unit.bad_adjectives.length)];
-		const secondName = unit.monsters_names_1[_.random(0, unit.monsters_names_1.length)];
-		const thirdName = unit.monsters_names_2[_.random(0, unit.monsters_names_2.length)];
-		this.unitName = `${firstName} ${secondName} ${thirdName}`;
+	generatePlayerUnit() {
+		this.generatePlayerName();
+	}
+	generateMonsterName() {
+		const firstName = unit.monsters.adjectives[_.random(0, unit.monsters.adjectives.length)];
+		const secondName = unit.monsters.names_1[_.random(0, unit.monsters.names_1.length)];
+		const thirdName = unit.monsters.names_2[_.random(0, unit.monsters.names_2.length)];
+		this.name = `${firstName} ${secondName} ${thirdName}`;
 	}
 
-	generatePlayerName() { // генерируем имя юнита игрока (имя игрока + прилагательное)
-		const firstName = unit.good_adjectives[_.random(0, unit.good_adjectives.length)];
+	generatePlayerName() {
+		const firstName = unit.players.adjectives[_.random(0, unit.players.adjectives.length)];
 		let secondName;
-		if (this.unitName) {
-			secondName = this.unitName;
+		if (this.name) {
+			secondName = this.name;
 		} else {
-			secondName = unit.players_names[_.random(0, unit.players_names.length)];
+			secondName = unit.players.names[_.random(0, unit.players.names.length)];
 		}
-		this.unitName = `${firstName} ${secondName}`;
+		this.name = `${firstName} ${secondName}`;
 	}
 
-	render() { // отрисовывает юнита
-		let head = document.createElement('div');
-		head.style.width = this.unitSprites.head.width + 'px';
-		head.style.height = this.unitSprites.head.height + 'px';
-		head.style.backgroundImage = `url(${this.unitSprites.head.path})`;
-		head.style.backgroundPositionX = this.unitSprites.head.bgPosX + 'px';
-		head.style.backgroundPositionY = this.unitSprites.head.bgPosY + 'px';
-		document.getElementById('unit').appendChild(head);
-	}
+	// render() { // отрисовывает юнита
+	// 	let head = document.createElement('div');
+	// 	head.style.width = this.unitSprites.head.width + 'px';
+	// 	head.style.height = this.unitSprites.head.height + 'px';
+	// 	head.style.backgroundImage = `url(${this.unitSprites.head.path})`;
+	// 	head.style.backgroundPositionX = this.unitSprites.head.bgPosX + 'px';
+	// 	head.style.backgroundPositionY = this.unitSprites.head.bgPosY + 'px';
+	// 	document.getElementById('unit').appendChild(head);
+	// }
 }
