2599ebd65577bae6de48e74a5b0e8ed8484091dc
Yury Staravoitau <MorfinAll@mail.ru>
2018-06-07 23:21:34 +0300

    fight/feature: added prevTarget and nextTarget selecting with green backround also added draw units in render function

--- src/js/modules/fight/fight.js
+++ src/js/modules/fight/fight.js
@@ -1,5 +1,4 @@
 import KeyboardController from '../KeyboardController/KeyboardController';
-import Utils from '../utils/utils';
 
 export default class Fight {
 	constructor(attacker, defender, generator) {
@@ -8,19 +7,26 @@ export default class Fight {
 		this.round = 0;
 		this.enableSelectingFlag = false;
 		this.isImpactDone = false;
+
 		this.selectedUnit = this.attacker[0];
+		this.activeUnit = this.attacker[0];
+
 		this.selectedUnitIndex = 0;
 		this.maxUnits = this.attacker.length + this.defender.length;
 
 		this.generatorlvl = generator;
-		console.log(this.generatorlvl);
+
+		this.canvas = document.getElementById('canvas');
+		this.ctx = this.canvas.getContext('2d');
 
 
 		this.gameLoopRunning = false;
 		if (this.isFightNotOver() === false) {
 			console.log('input data is wrong');
 		}
+
 		this.fightModulCycle();
+
 		return {
 			'attacker': this.attacker,
 			'defender': this.defender
@@ -94,11 +100,13 @@ export default class Fight {
 	updateSelecting() {
 		if (KeyboardController.pressedKeys.nextTarget) {
 			console.log('next target');
-			this.generatorlvl.next();
+			//this.generatorlvl.next();
 			KeyboardController.pressedKeys.nextTarget = false;
+			this.selectedUnit = this.nextTarget();
 		}
 		else {
 			if (KeyboardController.pressedKeys.prevTarget) {
+				this.selectedUnit = this.prevTarget();
 				console.log('prev target');
 				KeyboardController.pressedKeys.prevTarget = false;
 			}
@@ -107,16 +115,63 @@ export default class Fight {
 	}
 
 	render() {
-		console.log(this.defender[0].name + 'render');
-		// console.log('render');
-		Utils.testUnitRender(this.attacker[0], 200, 100);
-		Utils.testUnitRender(this.attacker[1], 0, 400);
-		Utils.testUnitRender(this.attacker[2], 400, 400);
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-		Utils.testUnitRender(this.defender[0], 1500, 100);
-		Utils.testUnitRender(this.defender[1], 1300, 400);
-		Utils.testUnitRender(this.defender[2], 1700, 400);
+		// draw selected backround
+		let x1, y1;
+		if (this.selectedUnit === this.attacker[0]) {
+			x1 = 200;
+			y1 = 100;
+		}
+		if (this.selectedUnit === this.attacker[1]) {
+			x1 = 0;
+			y1 = 400;
+		}
+		if (this.selectedUnit === this.attacker[2]) {
+			x1 = 400;
+			y1 = 400;
+		}
+		if (this.selectedUnit === this.defender[0]) {
+			x1 = 1500;
+			y1 = 100;
+		}
+		if (this.selectedUnit === this.defender[1]) {
+			x1 = 1300;
+			y1 = 400;
+		}
+		if (this.selectedUnit === this.defender[2]) {
+			x1 = 1700;
+			y1 = 400;
+		}
+		this.ctx.save();
+		this.ctx.fillStyle = 'green';
+		this.ctx.fillRect(x1, y1, 175, 275);
+
+		this.drawUnit(this.attacker[0], 200, 100);
+		this.drawUnit(this.attacker[1], 0, 400);
+		this.drawUnit(this.attacker[2], 400, 400);
+
+		this.drawUnit(this.defender[0], 1500, 100);
+		this.drawUnit(this.defender[1], 1300, 400);
+		this.drawUnit(this.defender[2], 1700, 400);
 	}
+
+	drawUnit(unit, posX = 0, posY = 0) {
+		const head = new Image();
+		const body = new Image();
+		const hands = new Image();
+		const legs = new Image();
+		head.src = unit.sprites.head.path;
+		hands.src = unit.sprites.hands.path;
+		legs.src = unit.sprites.legs.path;
+		body.src = unit.sprites.body.path;
+
+		this.ctx.drawImage(legs, unit.sprites.legs.sX, unit.sprites.legs.sY, unit.sprites.legs.width, unit.sprites.legs.height, unit.sprites.legs.dX + posX, unit.sprites.legs.dY + posY, unit.sprites.legs.width, unit.sprites.legs.height);
+		this.ctx.drawImage(body, unit.sprites.body.sX, unit.sprites.body.sY, unit.sprites.body.width, unit.sprites.body.height, unit.sprites.body.dX + posX, unit.sprites.body.dY + posY, unit.sprites.body.width, unit.sprites.body.height);
+		this.ctx.drawImage(head, unit.sprites.head.sX, unit.sprites.head.sY, unit.sprites.head.width, unit.sprites.head.height, unit.sprites.head.dX + posX, unit.sprites.head.dY + posY, unit.sprites.head.width, unit.sprites.head.height);
+		this.ctx.drawImage(hands, unit.sprites.hands.sX, unit.sprites.hands.sY, unit.sprites.hands.width, unit.sprites.hands.height, unit.sprites.hands.dX + posX, unit.sprites.hands.dY + posY, unit.sprites.hands.width, unit.sprites.hands.height);
+	}
+
 
 	isGroupAlive(groupOfUnits) {
 		return groupOfUnits.some(unit => unit.hp > 0);
@@ -133,14 +188,28 @@ export default class Fight {
 
 	nextTarget() {
 		this.selectedUnitIndex++;
-		if (this.selectedUnitIndex < this.attacker.length - 1) {
+		if (this.selectedUnitIndex > this.attacker.length * 2 - 1) {
+			this.selectedUnitIndex = 0;
+		}
+		if (this.selectedUnitIndex < this.attacker.length) {
 			return this.attacker[this.selectedUnitIndex];
 		}
 		else {
 			return this.defender[this.selectedUnitIndex - 3];
 		}
-
 	}
 
+	prevTarget() {
+		this.selectedUnitIndex--;
+		if (this.selectedUnitIndex < 0) {
+			this.selectedUnitIndex = this.attacker.length * 2 - 1;
+		}
+		if (this.selectedUnitIndex < this.attacker.length) {
+			return this.attacker[this.selectedUnitIndex];
+		}
+		else {
+			return this.defender[this.selectedUnitIndex - 3];
+		}
+	}
 
 }
