5e599b02d77eb5a63d984f11460d9614676fc24e
Artsiom Lipin <lipinartsiom@gmail.com>
2018-06-09 23:17:10 +0300

    userTask/feature: implemented audition task function, redesigned render task function, add check type flag function

--- src/js/game.js
+++ src/js/game.js
@@ -4,36 +4,5 @@ import Utils from './modules/utils/utils';
 import GameInterface from './modules/gameInterface/gameInterface';
 import UserTask from './modules/userTask/userTask';
 
-const x = new UserTask('sequence');
+const x = new UserTask();
 console.log(x);
-
-
-// const gameInterface = new GameInterface();
-// document.getElementById('startButt_id').addEventListener('click', () => {
-
-// 	function clearCanvas() {
-// 		const canvas = document.getElementById('canvas');
-// 		const ctx = canvas.getContext('2d');
-// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-// 	}
-// 	clearCanvas();
-
-// 	const player1 = new FightUnit('player');
-// 	const player2 = new FightUnit('player');
-// 	const player3 = new FightUnit('player');
-
-// 	const monster1 = new FightUnit('monster');
-// 	const monster2 = new FightUnit('monster');
-// 	const monster3 = new FightUnit('monster');
-
-
-// 	Utils.testUnitRender(player1, 200, 100);
-// 	Utils.testUnitRender(player2, 0, 400);
-// 	Utils.testUnitRender(player3, 400, 400);
-
-// 	Utils.testUnitRender(monster1, 1500, 100);
-// 	Utils.testUnitRender(monster2, 1300, 400);
-// 	Utils.testUnitRender(monster3, 1700, 400);
-
-// 	gameInterface.pressStartNewGameButton();
-// }, false);
--- src/js/modules/userTask/userTask.js
+++ src/js/modules/userTask/userTask.js
@@ -6,17 +6,25 @@ import taskConfig from '../../taskConfig';
 /*eslint no-case-declarations: 0*/
 
 export default class UserTask {
-	constructor(type = 'code') {
-		this.type = type;
+	constructor(type) {
+		this.type = this.checkTaskType(type);
 		this.impact = {};
 		this.generate();
 	}
+	checkTaskType(type) {
+		if (typeof type === 'string' && type === 'code' || type === 'translate' || type === 'sequence' || type === 'audition') {
+			return type;
+		} else {
+			return 'code';
+		}
+	}
 
 	generate() {
+		console.log(this.type);
+
 		const task = this.generateTask();
 		this.renderTask(task);
 		this.submitAnswer(task);
-
 	}
 
 	generateTask() {
@@ -34,20 +42,16 @@ export default class UserTask {
 		}
 	}
 
-
-	renderTask(task) {
+	generateCodeTask(task) {
 		const modalOverlay = document.createElement('div');
 		modalOverlay.className = 'modal-overlay';
 		modalOverlay.setAttribute('id', 'modal-overlay');
 		const taskModal = document.createElement('div');
 		taskModal.className = 'task-modal';
-
 		modalOverlay.appendChild(taskModal);
-
 		const title = document.createElement('h2');
 		title.className = 'task-modal-title';
 		taskModal.appendChild(title);
-
 		const question = document.createElement('div');
 		question.className = 'task-modal-question';
 		taskModal.appendChild(question);
@@ -61,67 +65,24 @@ export default class UserTask {
 		const answerVariantsList = document.createElement('ul');
 		answerVariantsList.className = 'task-modal-anwer-variants';
 
-		switch (this.type) {
-		case 'code':
-			title.textContent = 'Задача по JavaScript';
-			question.innerHTML = task[0];
+		title.textContent = 'Задача по JavaScript';
+		question.innerHTML = task[0];
 
-			for (let i = 0; i < task[1].length; i++) {
-				const answerVariantItem = document.createElement('li');
-				const label = document.createElement('label');
-				answerVariantItem.appendChild(label);
-				const input = document.createElement('input');
-				input.setAttribute('type', 'radio');
-				input.setAttribute('name', 'answer');
-				input.setAttribute('value', task[1][i]);
-				label.appendChild(input);
-				const inputText = document.createElement('code');
-				inputText.textContent = task[1][i];
-				label.appendChild(inputText);
-				answerVariantsList.appendChild(answerVariantItem);
-				answer.appendChild(answerVariantsList);
-				taskModal.appendChild(answer);
-			}
-
-			break;
-		case 'translate':
-			title.textContent = 'Перевод слова';
-			question.innerHTML = `Переведите с английского на русский слово: <code><b>${task[0]}</b></code>`;
-
+		for (let i = 0; i < task[1].length; i++) {
 			const answerVariantItem = document.createElement('li');
 			const label = document.createElement('label');
 			answerVariantItem.appendChild(label);
 			const input = document.createElement('input');
-			input.setAttribute('type', 'text');
+			input.setAttribute('type', 'radio');
 			input.setAttribute('name', 'answer');
+			input.setAttribute('value', task[1][i]);
 			label.appendChild(input);
+			const inputText = document.createElement('code');
+			inputText.textContent = task[1][i];
+			label.appendChild(inputText);
 			answerVariantsList.appendChild(answerVariantItem);
 			answer.appendChild(answerVariantsList);
 			taskModal.appendChild(answer);
-
-
-			break;
-		case 'sequence':
-			title.textContent = 'Правильная последовательность';
-			question.innerHTML = 'Расположите в правильной последовательности следующий код:';
-			const taskShuffle = _.shuffle(task);
-			for (let i = 0; i < task.length; i++) {
-				const answerVariantItem = document.createElement('li');
-				const pre = document.createElement('pre');
-				const inputText = document.createElement('code');
-				pre.appendChild(inputText);
-				inputText.textContent = taskShuffle[i];
-				answerVariantsList.classList.add('task-modal-answer-sequence');
-				answerVariantItem.appendChild(pre);
-				answerVariantsList.appendChild(answerVariantItem);
-				answer.appendChild(answerVariantsList);
-				taskModal.appendChild(answer);
-
-			}
-
-			break;
-		case 'audition':
-			break;
 		}
 
 		const button = document.createElement('button');
@@ -130,11 +91,186 @@ export default class UserTask {
 		button.setAttribute('type', 'submit');
 		button.textContent = 'sumbit';
 		taskModal.appendChild(button);
+		document.body.appendChild(modalOverlay);
+	}
 
+	generateTranslateTask(task) {
+		const modalOverlay = document.createElement('div');
+		modalOverlay.className = 'modal-overlay';
+		modalOverlay.setAttribute('id', 'modal-overlay');
+		const taskModal = document.createElement('div');
+		taskModal.className = 'task-modal';
+		modalOverlay.appendChild(taskModal);
+		const title = document.createElement('h2');
+		title.className = 'task-modal-title';
+		taskModal.appendChild(title);
+		const question = document.createElement('div');
+		question.className = 'task-modal-question';
+		taskModal.appendChild(question);
+
+		const answer = document.createElement('div');
+		answer.className = 'task-modal-answer';
+		const answerTitle = document.createElement('p');
+		answerTitle.className = 'task-modal-answer-title';
+		answerTitle.textContent = 'Ваш ответ:';
+		answer.appendChild(answerTitle);
+		const answerVariantsList = document.createElement('ul');
+		answerVariantsList.className = 'task-modal-anwer-variants';
+
+		title.textContent = 'Перевод слова';
+		question.innerHTML = `Переведите с английского на русский слово: <code><b>${task[0]}</b></code>`;
+
+		const answerVariantItem = document.createElement('li');
+		const label = document.createElement('label');
+		answerVariantItem.appendChild(label);
+		const input = document.createElement('input');
+		input.setAttribute('type', 'text');
+		input.setAttribute('name', 'answer');
+		label.appendChild(input);
+		answerVariantsList.appendChild(answerVariantItem);
+		answer.appendChild(answerVariantsList);
+		taskModal.appendChild(answer);
+
+		const button = document.createElement('button');
+		button.className = 'task-modal-btn';
+		button.setAttribute('id', 'task-answer');
+		button.setAttribute('type', 'submit');
+		button.textContent = 'sumbit';
+		taskModal.appendChild(button);
+		document.body.appendChild(modalOverlay);
+	}
+
+	generateSequenceTask(task) {
+		const modalOverlay = document.createElement('div');
+		modalOverlay.className = 'modal-overlay';
+		modalOverlay.setAttribute('id', 'modal-overlay');
+		const taskModal = document.createElement('div');
+		taskModal.className = 'task-modal';
+		modalOverlay.appendChild(taskModal);
+		const title = document.createElement('h2');
+		title.className = 'task-modal-title';
+		taskModal.appendChild(title);
+		const question = document.createElement('div');
+		question.className = 'task-modal-question';
+		taskModal.appendChild(question);
+
+		const answer = document.createElement('div');
+		answer.className = 'task-modal-answer';
+		const answerTitle = document.createElement('p');
+		answerTitle.className = 'task-modal-answer-title';
+		answerTitle.textContent = 'Ваш ответ:';
+		answer.appendChild(answerTitle);
+		const answerVariantsList = document.createElement('ul');
+		answerVariantsList.className = 'task-modal-anwer-variants';
+
+		title.textContent = 'Правильная последовательность';
+		question.innerHTML = 'Расположите в правильной последовательности следующий код:';
+		const taskShuffle = _.shuffle(task);
+		for (let i = 0; i < task.length; i++) {
+			const answerVariantItem = document.createElement('li');
+			const pre = document.createElement('pre');
+			const inputText = document.createElement('code');
+			pre.appendChild(inputText);
+			inputText.textContent = taskShuffle[i];
+			answerVariantsList.classList.add('task-modal-answer-sequence');
+			answerVariantItem.appendChild(pre);
+			answerVariantsList.appendChild(answerVariantItem);
+			answer.appendChild(answerVariantsList);
+			taskModal.appendChild(answer);
+		}
+
+		const button = document.createElement('button');
+		button.className = 'task-modal-btn';
+		button.setAttribute('id', 'task-answer');
+		button.setAttribute('type', 'submit');
+		button.textContent = 'sumbit';
+		taskModal.appendChild(button);
 		document.body.appendChild(modalOverlay);
 		$('.task-modal-anwer-variants').sortable();
-
 	}
+
+	generateAuditionTask(task) {
+		const modalOverlay = document.createElement('div');
+		modalOverlay.className = 'modal-overlay';
+		modalOverlay.setAttribute('id', 'modal-overlay');
+		const taskModal = document.createElement('div');
+		taskModal.className = 'task-modal';
+		modalOverlay.appendChild(taskModal);
+		const title = document.createElement('h2');
+		title.className = 'task-modal-title';
+		taskModal.appendChild(title);
+		const question = document.createElement('div');
+		question.className = 'task-modal-question';
+		taskModal.appendChild(question);
+
+		const answer = document.createElement('div');
+		answer.className = 'task-modal-answer';
+		const answerTitle = document.createElement('p');
+		answerTitle.className = 'task-modal-answer-title';
+		answerTitle.textContent = 'Ваш ответ:';
+		answer.appendChild(answerTitle);
+		const answerVariantsList = document.createElement('ul');
+		answerVariantsList.className = 'task-modal-anwer-variants';
+		title.textContent = 'Аудирование';
+		question.innerHTML = 'Ваша задача написать воспроизведенное слово. Нажмите на конопку, чтобы воспроизвести.';
+		const btn = document.createElement('button');
+		btn.className = 'btn';
+		btn.textContent = 'Воспроизвести';
+
+		const awaitVoices = new Promise(done =>
+			window.speechSynthesis.onvoiceschanged = done);
+		const speakTaskWord = () => {
+			awaitVoices.then(() => {
+				const synth = window.speechSynthesis;
+				const speech = new SpeechSynthesisUtterance(task);
+				let voices = [];
+				voices = synth.getVoices();
+				speech.voice = voices[8];
+				speech.rate = 0.8;
+				synth.speak(speech);
+			});
+		};
+
+		btn.onclick = speakTaskWord;
+		question.appendChild(btn);
+
+		const answerVariantItem = document.createElement('li');
+		const label = document.createElement('label');
+		answerVariantItem.appendChild(label);
+		const input = document.createElement('input');
+		input.setAttribute('type', 'text');
+		input.setAttribute('name', 'answer');
+		label.appendChild(input);
+		answerVariantsList.appendChild(answerVariantItem);
+		answer.appendChild(answerVariantsList);
+		taskModal.appendChild(answer);
+
+		const button = document.createElement('button');
+		button.className = 'task-modal-btn';
+		button.setAttribute('id', 'task-answer');
+		button.setAttribute('type', 'submit');
+		button.textContent = 'sumbit';
+		taskModal.appendChild(button);
+		document.body.appendChild(modalOverlay);
+	}
+
+	renderTask(task) {
+		switch (this.type) {
+		case 'code':
+			this.generateCodeTask(task);
+			break;
+		case 'translate':
+			this.generateTranslateTask(task);
+			break;
+		case 'sequence':
+			this.generateSequenceTask(task);
+			break;
+		case 'audition':
+			this.generateAuditionTask(task);
+			break;
+		}
+	}
+
 	checkAnswer(task) {
 		let userAnswer;
 		let results;
@@ -144,12 +280,11 @@ export default class UserTask {
 			if (userAnswer !== null) {
 				if (userAnswer.value === task[2]) {
 					results = true;
-					console.log(results);
 				} else {
 					results = false;
-					console.log(results);
 				}
 			}
+			console.log(results);
 			break;
 		case 'translate':
 			userAnswer = document.querySelector('input[name=answer]');
@@ -172,18 +307,25 @@ export default class UserTask {
 				results.push(userAnswer.children[i].innerText.trim());
 				taskAnswer.push(task[i].trim());
 			}
-
 			taskAnswer = taskAnswer.join('');
 			results = results.join('');
-
 			if (results === taskAnswer) {
-				console.log(true);
+				results = true;
 			} else {
-				console.log(false);
-
+				results = false;
 			}
+			console.log(results);
 			break;
 		case 'audition':
+			userAnswer = document.querySelector('input[name=answer]');
+			if (userAnswer !== null) {
+				if (userAnswer.value.trim() === task[0]) {
+					results = true;
+				} else {
+					results = false;
+				}
+			}
+			console.log(results);
 			break;
 		}
 
@@ -192,7 +334,6 @@ export default class UserTask {
 	submitAnswer(task) {
 		const modal = document.getElementById('modal-overlay');
 		const submit = document.getElementById('task-answer');
-
 		modal.addEventListener('click', (e) => {
 			if (e.target === submit) {
 				this.checkAnswer(task);
