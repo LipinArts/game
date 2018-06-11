5b444972d2e414ae3f7d7c91b28ab492a3ebc872
Artsiom Lipin <lipinartsiom@gmail.com>
2018-06-12 00:39:39 +0300

    selectionWheelModal/feature: created markup selection wheel modal, add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
 	"env": {
 		"browser": true,
 		"es6": true,
@@ -28,34 +27,4 @@ module.exports = {
 			"always"
 		]
 	}
-=======
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": 2017
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
->>>>>>> 14a93b73ee827d3f73b18f95083d6a35f6613d7d
 };
--- src/js/game.js
+++ src/js/game.js
@@ -1,12 +1,5 @@
 import GameInterface from './modules/gameInterface/gameInterface';
-<<<<<<< HEAD
-import UserTask from './modules/userTask/userTask';
-
-const x = new UserTask();
-console.log(x);
-=======
 const gameInterface = new GameInterface();
 document.getElementById('startButt_id').addEventListener('click', () => {
 	gameInterface.pressStartNewGameButton();
 }, false);
->>>>>>> 14a93b73ee827d3f73b18f95083d6a35f6613d7d
